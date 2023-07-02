5489206bd3832063afa1580f68f416b2bbca74d2
Tony <tony@Tonys-MacBook-Pro.local>
2023-07-02 20:54:41 +0700

    update bundle code

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
 const app = express();
-app.get("/api/hello", (req, res) => res.send("Hi"));
+app.get("/api/hello", (_, res) => res.send("Hi"));
+app.use(express.static(process.cwd() + "/dist")); // running when use vite bundle code
 app.listen(2001, () => console.log("Started"));
